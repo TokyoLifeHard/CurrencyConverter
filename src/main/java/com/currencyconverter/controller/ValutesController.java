7fadde6fa509f21f0cabe3536d00bc6182fc7a26
Kawaiiskii <antonzhadnov@gmail.com>
2023-04-04 19:38:11 +0300

    add GET metod to controller

--- src/main/java/com/currencyconverter/controller/ValutesController.java
+++ src/main/java/com/currencyconverter/controller/ValutesController.java
@@ -4,20 +4,21 @@ import com.currencyconverter.cache.CacheService;
 import com.currencyconverter.entity.Valute;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api/valutes")
+@RequestMapping("/api/valute")
 public class ValutesController {
 
     @Autowired
     CacheService cacheService;
 
-    @GetMapping
-    public Valute getValute(){
-        return null;
+    @GetMapping("/{name}")
+    public Valute getValute(@PathVariable String name){
+        return cacheService.getValute(name);
     }
 
 }
