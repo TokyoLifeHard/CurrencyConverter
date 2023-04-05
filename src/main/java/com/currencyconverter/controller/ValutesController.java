0dc184a0d9b84429a4feb18fab2f2e4efc7e6995
Kawaiiskii <antonzhadnov@gmail.com>
2023-04-05 23:21:43 +0300

    add caclCurse

--- src/main/java/com/currencyconverter/controller/ValutesController.java
+++ src/main/java/com/currencyconverter/controller/ValutesController.java
@@ -3,10 +3,15 @@ package com.currencyconverter.controller;
 import com.currencyconverter.cache.CacheService;
 import com.currencyconverter.entity.Valute;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -21,4 +26,21 @@ public class ValutesController {
         return cacheService.getValute(name);
     }
 
+    @PostMapping(path = "/calc_curse" ,produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<Map<String,String>> caclCurse(@RequestBody Map<String,String> params){
+
+        Valute fromValute = cacheService.getValute(params.get("from"));
+        Valute toValute = cacheService.getValute(params.get("to"));
+
+        BigDecimal fromdecim = new BigDecimal(fromValute.getValue().replace(",","."));
+        BigDecimal todecim = new BigDecimal(toValute.getValue().replace(",","."));
+
+        BigDecimal curse = fromdecim.divide(todecim, RoundingMode.UP);
+        Map<String,String> resp = new HashMap<>();
+        resp.put("message","for 1 "+fromValute.getCharCode()+" your'll get "+curse.toString()+" "+ toValute.getCharCode());
+        return new ResponseEntity<Map<String,String>>(resp, HttpStatus.valueOf(200));
+
+
+    }
 }
