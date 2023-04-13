cf71df23a62635760758b5f51cdc6d8e5bf55b7f
Kawaiiskii <antonzhadnov@gmail.com>
2023-04-13 20:15:54 +0300

    add beforeCalcValutes metod

--- src/main/java/com/currencyconverter/aspects/ValutesControllerLoggingAspect.java
+++ src/main/java/com/currencyconverter/aspects/ValutesControllerLoggingAspect.java
@@ -1,7 +1,18 @@
 package com.currencyconverter.aspects;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
+@Aspect
 public class ValutesControllerLoggingAspect {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Before("execution(public * caclCurse(*))")
+    public void beforeCalcValutes(){
+        logger.info("exec calcCurse metod");
+    }
 }
